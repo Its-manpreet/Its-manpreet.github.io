5ffeee4593d367475bebfd24fa59f1b576306687
Its-manpreet <92618484+Its-manpreet@users.noreply.github.com>
2023-03-14 12:50:38 +0530

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -5,11 +5,20 @@ const projects = [
         "url": "https://dsc.gg/quoter-bot"
     },
     {
-        "name": "Minearth SMP",
+        "name": "Minearth SMP 2",
         "description": "A Minecraft SMP to have fun!",
-        "url": "join.manpreet.tk"
+        "url": "manpreet.tk"
     },
-
+    {
+        "name": "Minearth SMP 3",
+        "description": "A Minecraft java modded SMP to have fun!",
+        "url": "smp.manpreet.tk"
+    },
+    {
+        "name": "CDN/Media server",
+        "description": "A simple server delivering files",
+        "url": "cdn.manpreet.tk"
+    },
 
 
 
