3a2f6872a1a26cbdf9338e569567d0929ac4a3ad
Its-manpreet <92618484+Its-manpreet@users.noreply.github.com>
2023-07-12 09:50:19 +0530

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -4,20 +4,35 @@ const projects = [
         "description": "A cool discord bot with a lot of quotes.",
         "url": "https://dsc.gg/quoter-bot"
     },
+        {
+        "name": "Minearth SMP",
+        "description": "A Minecraft SMP to have fun!",
+        "url": "https://dsc.gg/chillclubop"
+    },
     {
         "name": "Minearth SMP 2",
-        "description": "A Minecraft SMP to have fun!",
-        "url": "manpreet.tk"
+        "description": "A Minecraft SMP with custom stuff to have fun!",
+        "url": "https://dsc.gg/chillclubop"
     },
     {
         "name": "Minearth SMP 3",
         "description": "A Minecraft java modded SMP to have fun!",
-        "url": "smp.manpreet.tk"
+        "url": "https://dsc.gg/chillclubop"
+    },
+        {
+        "name": "Minearth SMP 4",
+        "description": "Minearth season 2 but better!",
+        "url": "https://dsc.gg/chillclubop"
     },
     {
         "name": "CDN/Media server",
-        "description": "A simple server delivering files",
-        "url": "cdn.manpreet.tk"
+        "description": "A simple server delivering files.",
+        "url": "https://github.com/Its-manpreet/cdn-server"
+    },
+        {
+        "name": "Task automator",
+        "description": "A program that runs js scripts on a specific event",
+        "url": "https://github.com/Its-manpreet/task-automation"
     },
 
 
